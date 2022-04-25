07a8259e85d02747ccec4970b97a2167fda56707
manzomonza <massimiliano.manzo@posteo.ch>
2022-04-25 17:49:19 +0200

    Added GXS functionality in R script

--- variantCollection.R
+++ variantCollection.R
@@ -104,11 +104,11 @@ metavariants = aggregator(opt$file)
 
 
 
-exon.gr <- ""
-metavariants = aggregator("/Volumes/GoogleDrive/.shortcut-targets-by-id/1yuFiN1dlcUgo1_ELdNVXegTfB61oDv8G/Patientendaten/2022/W0501-W0550/W0510_PrecisionDNA/Snvindel.tsv")
-
-precisionInfo()
-
+# exon.gr <- ""
+# metavariants = aggregator("/Volumes/GoogleDrive/.shortcut-targets-by-id/1yuFiN1dlcUgo1_ELdNVXegTfB61oDv8G/Patientendaten/2022/W0501-W0550/W0510_PrecisionDNA/Snvindel.tsv")
+# 
+# precisionInfo()
+# 
 
 
 if(nrow(metavariants$snv) > 0){
