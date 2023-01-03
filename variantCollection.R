dade254548bf179e3bdf4ad43009b0302a32ea2e
manzomonza <massimiliano.manzo@posteo.ch>
2023-01-03 18:20:50 +0100

    update variantCollection.R

--- variantCollection.R
+++ variantCollection.R
@@ -1,35 +1,12 @@
-#  watchdog output based variant aggregation:
-# Strategy becomes to extract as much as possible from watchdog folder, including metadata. Known exceptions are:
-# Precision panels:
-#   - will need to be checked one directory above watchdog
-#   - maybe does not have info.csv --> Extract filename from path, rest metadata NA
-# State: April 2022
-
-## Change of strategy: Use watchdog folders instead
-
+# # watchdog output based variant aggregation:
+# # Strategy becomes to extract as much as possible from watchdog folder, including metadata. Known exceptions are:
+# # Precision panels:
+# #   - will need to be checked one directory above watchdog
+# #   - maybe does not have info.csv --> Extract filename from path, rest metadata NA
+# # State: April 2022
 library(magrittr)
 library(optparse)
-library(variantCollection)
 
-
-filteredFile <- "/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_FILTERED.tsv"
-snvFile <- "/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_SNV.tsv"
-cnvFile <- "/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_CNV.tsv"
-
-
-### To be selected columns
-filtered_cols <- c("amino_acid_change","analysisDate","analysisName","cds_region","cds_region_No",
-                   "cnv_confidence", "coding", "copy_number", "dirpath",
-                   "exon", "exportDate", "gene","location","locus","multiply_freq_by_100",
-                   "percent_frequency","transcript","type","workflowName")
-
-cnv_cols <- c("analysisDate","analysisName","chromosome","copy_number","dirpath","exportDate",
-              "five","fivePercent_conf","gene","locus","ninetyfive","ninetyfivePercent_conf", 
-              "workflowName")
-
-snv_cols <- c("amino_acid_change","analysisDate","analysisName","cds_region","cds_region_No","clinvar_ready_AA",
-              "coding","dirpath","exon","exportDate","gene","IR_clinvar","location","locus","multiply_freq_by_100",
-              "one_AA","percent_frequency","three_AA","transcript","type","workflowName")
 
 ### OPT Parse
 option_list = list(
@@ -41,6 +18,101 @@ opt = parse_args(opt_parser)
 
 print(opt$watchdir)
 
+
+readInfo <- function(infopath){
+  info = readr::read_csv(infopath) %>%
+    tidyr::separate(col = 1,
+                    into = c("No.", "metadata"), sep = "^\\d{1,} ") %>%
+    tidyr::separate(col = "metadata",
+                    into = c("metadata", "value"), sep = "=") %>%
+    dplyr::select(-No.) %>%
+    dplyr::mutate(value = stringr::str_remove_all(string = value, pattern = "NA| ")) %>%
+    tidyr::pivot_wider(names_from = metadata,
+                       values_from = value)
+  if('chromosome' %in% colnames(info)){
+    info = info %>% dplyr::rename(Chr = chromosome)
+    
+  }
+  return(info)
+}
+
+
+precisionInfo <- function(filepath){
+  info = readr::read_csv(filepath, col_names = TRUE) %>%
+    tidyr::separate(col = `Software Version Details`,
+                    into = c("parameter", "value"),
+                    sep = ",")
+  info = info %>%
+    as.data.frame() %>%
+    dplyr::filter(grepl("Sample Name|^Name$|Start Date|Completion Date",parameter, ignore.case = TRUE)) %>%
+    dplyr::filter(value != "<NA>" & value != "Assay Name" & value != "MT216761" & !grepl("gnxs--0036", value)) %>%
+    tidyr::pivot_wider(names_from = parameter, values_from = value) %>%
+    dplyr::rename(analysisName = `Sample Name`,
+                  workflowName = Name,
+                  analysisDate = `Start Date`,
+                  exportDate = `Completion Date`)
+  return(info)
+}
+
+
+variantCollection <- function(watchdogDir){
+  if(dir.exists(watchdogDir)){
+    #print("dir exists")
+    files <- list.files(path = watchdogDir, full.names = TRUE)
+    # grep different filepaths
+    file_list <- list(cnv = grep("prep_cnv", files, value = TRUE),
+                      snv = grep("prep_snv", files, value = TRUE),
+                      filtered = grep("prep_filtered", files, value = TRUE))
+    dirpath <- rev(stringr::str_split(dirname(watchdogDir), pattern = "/", simplify = TRUE))[1]
+    
+    
+    if(grepl("Snvindel_watchdog", watchdogDir)){
+      #print("Dir is from precision")
+      infopath = paste0(watchdogDir, "/GNXS_metadata.txt")
+      if(file.exists(infopath)){
+        #print('infopath exists')
+        info = precisionInfo(infopath)
+      }else{
+        #print('infopath does not exist')
+        info = data.frame(workflowName = "Precision",
+                          analysisName = dirpath,
+                          exportDate = NA,
+                          analysisDate = NA)
+      }
+    }else{
+      file_list$info = grep("Info.csv", files, value = TRUE)
+      info = readInfo(file_list$info)
+    }
+    
+    info$dirpath = dirpath
+    #print('dirpath')
+    cnv = dplyr::bind_cols(readr::read_tsv(file_list$cnv), info) %>% 
+      dplyr::mutate(across(.cols = everything(), .fns = as.character))
+    #print('cnv pass')
+    snv = dplyr::bind_cols(readr::read_tsv(file_list$snv), info) %>%
+      dplyr::mutate(across(.cols = everything(), .fns = as.character))
+    
+    filtered = dplyr::bind_cols(readr::read_tsv(file_list$filtered), info) %>%
+      dplyr::mutate(across(.cols = everything(), .fns = as.character))
+    return(list(snv = snv,
+                cnv = cnv,
+                filtered = filtered))
+  }
+}
+
+snv_cols <- c("amino_acid_change","analysisDate","analysisName","cds_region","cds_region_No","clinvar_ready_AA",
+              "coding","dirpath","exon","exportDate","gene","IR_clinvar","location","locus","multiply_freq_by_100",
+              "one_AA","percent_frequency","three_AA","transcript","type","workflowName")
+
+cnv_cols <- c("analysisDate","analysisName","chromosome","copy_number","dirpath","exportDate",
+              "five","fivePercent_conf","gene","locus","ninetyfive","ninetyfivePercent_conf", 
+              "workflowName")
+
+filtered_cols <- c("amino_acid_change","analysisDate","analysisName","cds_region","cds_region_No",
+                   "cnv_confidence", "coding", "copy_number", "dirpath",
+                   "exon", "exportDate", "gene","location","locus","multiply_freq_by_100",
+                   "percent_frequency","transcript","type","workflowName")
+
 ## Function call
 metavariants <- variantCollection(opt$watchdir)
 
@@ -49,12 +121,27 @@ metavariants$snv <- metavariants$snv %>% dplyr::select(all_of(snv_cols))
 metavariants$cnv <- metavariants$cnv %>% dplyr::select(all_of(cnv_cols))
 metavariants$filtered <- metavariants$filtered %>% dplyr::select(all_of(filtered_cols))
 
-# Write out -- SNV
-writeVariants(tableIn = metavariants$snv,
-              tableOut = snvFile)
-# Write out --  CNV
-writeVariants(tableIn = metavariants$cnv,
-              tableOut = cnvFile)
-# Write out --  FILTERED
-writeVariants(tableIn = metavariants$filtered,
-              tableOut = snvFiltered)
+
+if(nrow(metavariants$snv) > 0){
+  if(file.exists("/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_SNV.tsv")){
+    readr::write_tsv(metavariants$snv, "/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_SNV.tsv", append = TRUE, col_names = TRUE)
+  }else{
+    readr::write_tsv(metavariants$snv, "/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_SNV.tsv", append = FALSE, col_names = TRUE)
+  }
+}
+
+if(nrow(metavariants$cnv) > 0){
+  if(file.exists("/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_CNV.tsv")){
+    readr::write_tsv(metavariants$cnv, "/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_CNV.tsv", append = TRUE, col_names = TRUE)
+  }else{
+    readr::write_tsv(metavariants$cnv, "/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_CNV.tsv", append = FALSE, col_names = TRUE)
+  }
+}
+
+if(nrow(metavariants$filtered) > 0){
+  if(file.exists("/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_FILTERED.tsv")){
+    readr::write_tsv(metavariants$filtered, "/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_FILTERED.tsv", append = TRUE, col_names = TRUE)
+  }else{
+    readr::write_tsv(metavariants$filtered, "/mnt/NGS_Diagnostik/SampleCentricVariantCollection/Sample_centric_FILTERED.tsv", append = FALSE, col_names = TRUE)
+  }
+}
