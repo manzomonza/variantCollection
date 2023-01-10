61484f2299bac94ad7cd33996cac50ede8db477c
manzomonza <massimiliano.manzo@posteo.ch>
2023-01-10 11:27:32 +0100

    Correct spelling error in cancerHotspot column selection

--- VariantCollection/SampleCentricTables_paths.R
+++ VariantCollection/SampleCentricTables_paths.R
@@ -58,6 +58,6 @@ variant_interpretations_parse <- function(variant_ints_path){
                                         !is.na(tsgInfo) |
                                         !is.na(cancerHotspot))
   #variants$gene = ifelse(is.na(variants$gene), variants$genes, variants$gene)
-  variants = variants %>% dplyr::select(gene, coding, one_AA, COSMIC_n_total, cancerHotspots, BIMI_variant, Kommentar )
+  variants = variants %>% dplyr::select(gene, coding, one_AA, COSMIC_n_total, cancerHotspot, BIMI_variant, Kommentar )
   return(variants)
 }
